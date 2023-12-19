b10cf1261feebbee39960b78a8e83cd02827ca55
SH-ur <sdjsm2003@gmail.com>
2023-12-19 11:26:07 -0400

    Icons y primera parte de CSS

--- src/Views/Contact.jsx
+++ src/Views/Contact.jsx
@@ -1,7 +1,16 @@
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import '../css/Contact.css'
+
 export const Contact =()=>{
     return (
-        <div>
-            <strong>Por acá irán los distintos medios de contacto</strong>
+        <div style={{marginLeft: '535px', border: 'solid blue 20px'}}>
+            <strong style={{textAlign: 'center', color: 'golden'}}>Por acá irán los distintos medios de contacto</strong>
+
+            <div className='contactIcons'>
+               <a href="https://github.com/SH-ur" target="_blank" rel="noreferrer"> <AiFillGithub size='100px' color='yellow'/> </a>
+<a href='https://www.linkedin.com/in/santiago-soto-43ab99260/' target='_blank' rel='noreferrer'> <AiFillLinkedin size='100px' color='blue'/></a>
+
+            </div>
         </div>
     )
 }
