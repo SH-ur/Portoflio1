e49efe1640e1c273d1e137944e9d8a70ecefe72e
SH-ur <sdjsm2003@gmail.com>
2024-01-12 14:11:19 -0400

    Algunos cambios a los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import { useState } from 'react'
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation} from "react-router-dom";
 import { Home } from "./Views/Home";
 import { Proyects } from "./Views/Proyects";
 import { Contact } from "./Views/Contact";
@@ -10,12 +10,14 @@ import "./css/Navbar.css";
 import "./css/App2.css"
 function App() {
   //const [count, setCount] = useState(0)
-
+const location = useLocation();
   return (
     
       <div className="main-Items">
         
           <Navbar classname='sidebar'/>
+          {location.pathname !== '/' ? '' : <p>Si no ves nada,
+       dale click a uno de los íconos</p>}
     
         <Routes>
           <Route path="/home" element={<Home />} />
